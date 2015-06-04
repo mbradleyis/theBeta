30fe2bd14783653d680c7c66bdff271db2e25167
Will <cameron.will@gmail.com>
2015-06-04 11:13:26 -0700

    first commit

---
+++ app/src/app.js
@@ -0,0 +1,7 @@
+var React = require('react');
+var Test = require('./components/Test.jsx');
+
+React.render(
+  <Test />,
+  document.getElementById('example')
+);
---
+++ app/src/components/Test.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+
+var Test = React.createClass({
+
+  render: function () {
+    return  (
+      <div>
+        <h1>Hello You</h1>
+        <p>Still works.</p>
+      </div>
+    );
+  }
+
+});
+
+module.exports = Test;
